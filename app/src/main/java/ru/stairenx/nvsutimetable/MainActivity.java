06f0ee87aede04ac071b22cfa7a991b5f485a025
Lev <dremamax@gmail.com>
2019-04-08 23:58:45 +0500

    Обновление

--- app/src/main/java/ru/stairenx/nvsutimetable/MainActivity.java
+++ app/src/main/java/ru/stairenx/nvsutimetable/MainActivity.java
@@ -1,9 +1,14 @@
 package ru.stairenx.nvsutimetable;
 
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -19,38 +24,66 @@ import ru.stairenx.nvsutimetable.server.WebAction;
 public class MainActivity extends AppCompatActivity {
 
     public static List<PairItem> data = new ArrayList<>();
-    private static RecyclerView rv;
-    private RecyclerView.LayoutManager lm;
-    private Button btn;
-    private EditText group;
+    private static RecyclerView RecyclerView;
+    private RecyclerView.LayoutManager LayoutManager;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        group = findViewById(R.id.numGroup);
-        btn = findViewById(R.id.btn);
-        initBtn();
-        rv = (RecyclerView) findViewById(R.id.recyclerView);
-        rv.setHasFixedSize(true);
-        lm = new LinearLayoutManager(this);
-        rv.setLayoutManager(lm);
+        initToolbarAndSnackBar();
+        RecyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        RecyclerView.setHasFixedSize(true);
+        LayoutManager = new LinearLayoutManager(this);
+        RecyclerView.setLayoutManager(LayoutManager);
         PairAdapter adapter = new PairAdapter(data);
-        rv.setAdapter(adapter);
+        RecyclerView.setAdapter(adapter);
     }
 
     public static void update(){
         PairAdapter adapter = new PairAdapter(data);
-        rv.setAdapter(adapter);
+        RecyclerView.setAdapter(adapter);
     }
 
-    private void initBtn(){
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String gr = group.getText().toString();
-                new WebAction.getBook().execute(gr);
-            }
-        });
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_scrolling, menu);
+        return true;
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_user) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+     private void initToolbarAndSnackBar(){
+         toolbar = (Toolbar) findViewById(R.id.toolbar);
+         toolbar.setTitle("");
+         setSupportActionBar(toolbar);
+         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+         fab.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View view) {
+                 Snackbar.make(view, "Расписание на сегодня", Snackbar.LENGTH_LONG)
+                         .setAction("Action", null).show();
+                 new WebAction.getBook().execute("3702");
+                setToolbarTitle("Сегодня + Дата");//потом сделать чтобы сюда дата сама ставилась
+             }
+         });
+     }
+
+     private void setToolbarTitle(String title){
+        if(toolbar != null)
+            toolbar.setTitle(title);
+     }
 }
