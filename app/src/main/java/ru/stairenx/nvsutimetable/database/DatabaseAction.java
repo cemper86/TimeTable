dd13fd9c69d9ad4e263c66bdac874dd2a21f63e7
viergo <cemper86@gmail.com>
2019-04-11 07:43:01 +0500

    м

--- app/src/main/java/ru/stairenx/nvsutimetable/database/DatabaseAction.java
+++ app/src/main/java/ru/stairenx/nvsutimetable/database/DatabaseAction.java
@@ -15,32 +15,32 @@ import static nl.qbusict.cupboard.CupboardFactory.cupboard;
 
 public class DatabaseAction {
 
-    private static SQLiteDatabase sql;
+    private static SQLiteDatabase sqLiteDatabase;
     private static Context context;
 
     public static void addUser(String faculty, String mGroup){
         DatabaseHelper dbHelper = new DatabaseHelper(getContext());
-        sql = dbHelper.getWritableDatabase();
+        sqLiteDatabase = dbHelper.getWritableDatabase();
         UserTable user = new UserTable(faculty, mGroup);
-        cupboard().withDatabase(sql).put(user);
+        cupboard().withDatabase(sqLiteDatabase).put(user);
     }
 
     public static void addUser(String faculty, String group, String subgroup){
         DatabaseHelper dbHelper = new DatabaseHelper(getContext());
-        sql =dbHelper.getWritableDatabase();
+        sqLiteDatabase =dbHelper.getWritableDatabase();
         UserTable user = new UserTable(faculty, group, subgroup);
-        cupboard().withDatabase(sql).put(user);
+        cupboard().withDatabase(sqLiteDatabase).put(user);
     }
 
     public static String getUserGroup(){
         String group = new String();
         DatabaseHelper dbHelper = new DatabaseHelper(getContext());
-        sql = dbHelper.getWritableDatabase();
+        sqLiteDatabase = dbHelper.getWritableDatabase();
 
         String table = getNameTable(UserTable.class);
         if (getCountTable(table)){
             String textQuery = "SELECT * FROM " + table + " ORDER BY " + DatabaseConstants.COLUMN_ID + " DESC";
-            Cursor cursor = sql.rawQuery(textQuery, null);
+            Cursor cursor = sqLiteDatabase.rawQuery(textQuery, null);
             while (cursor.moveToNext()) {
                 group = cursor.getString(cursor.getColumnIndex("group"));
             }
@@ -48,7 +48,7 @@ public class DatabaseAction {
         }else{
             group = ConstantsNVSU.NONE;
         }
-        sql.close();
+        sqLiteDatabase.close();
 
         return group;
     }
@@ -56,8 +56,8 @@ public class DatabaseAction {
     public static boolean getCountTable(String table){
         String textQuery = "SELECT * FROM " + table;
         DatabaseHelper dbHelper = new DatabaseHelper(getContext());
-        sql = dbHelper.getWritableDatabase();
-        Cursor cursor = sql.rawQuery(textQuery, null);
+        sqLiteDatabase = dbHelper.getWritableDatabase();
+        Cursor cursor = sqLiteDatabase.rawQuery(textQuery, null);
         int q = cursor.getCount();
         if(q>0){
             return true;
@@ -76,24 +76,24 @@ public class DatabaseAction {
         int id;
         List<UserItem> info = new ArrayList<>();
         DatabaseHelper dbHelper = new DatabaseHelper(getContext());
-        sql = dbHelper.getWritableDatabase();
+        sqLiteDatabase = dbHelper.getWritableDatabase();
         String table = getNameTable(UserTable.class);
         String textQuery = "SELECT * FROM "+table+" WHERE group='"+group+"';";
-        Cursor cursor = sql.rawQuery(textQuery, null);
+        Cursor cursor = sqLiteDatabase.rawQuery(textQuery, null);
         cursor.moveToFirst();
         id = cursor.getInt(cursor.getColumnIndex(DatabaseConstants.COLUMN_ID));
         cursor.close();
         Long idL = Long.valueOf(id);
-        UserTable item = cupboard().withDatabase(sql).get(UserTable.class, idL);
+        UserTable item = cupboard().withDatabase(sqLiteDatabase).get(UserTable.class, idL);
         info.add(new UserItem(item.faculty, item.group, item.subgroup));
         return info;
     }
 
     public static void deleteUser(int id){
         DatabaseHelper dbHelper = new DatabaseHelper(getContext());
-        sql = dbHelper.getWritableDatabase();
+        sqLiteDatabase = dbHelper.getWritableDatabase();
         Long idL = Long.valueOf(id);
-        cupboard().withDatabase(sql).delete(UserTable.class,idL);
+        cupboard().withDatabase(sqLiteDatabase).delete(UserTable.class,idL);
     }
 
     public static Context getContext(){
